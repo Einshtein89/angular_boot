8825a318ce156b65cf23fad15ebb1bc6595f3d56
Evgen <evgen.makhnonosov@nixsolutions.com>
2018-05-25 21:22:57 +0300

    initial commit

---
+++ server/src/main/java/com/nixsolutions/angular_boot/BackboneBootApplication.java
@@ -0,0 +1,17 @@
+package com.nixsolutions.angular_boot;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackboneBootApplication {
+
+	private static final Logger log = LoggerFactory.getLogger(BackboneBootApplication.class);
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackboneBootApplication.class, args);
+	}
+
+}
