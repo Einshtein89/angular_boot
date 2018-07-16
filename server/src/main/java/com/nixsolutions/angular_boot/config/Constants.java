cd2b00f659626aa409632c108afcb6634eee856a
Evgen Makhnonosov <evgen.makhnonosov@.nixsolutions.com>
2018-07-16 20:27:52 +0300

    initial changes for adding security and JWT token

--- server/src/main/java/com/nixsolutions/angular_boot/config/Constants.java
+++ server/src/main/java/com/nixsolutions/angular_boot/config/Constants.java
@@ -2,4 +2,8 @@ package com.nixsolutions.angular_boot.config;
 
 public class Constants {
     public static final String USER_ROLE = "USER";
+    public static final long ACCESS_TOKEN_VALIDITY_SECONDS = 5*60*60;
+    public static final String SIGNING_KEY = "devglan123r";
+    public static final String TOKEN_PREFIX = "Bearer ";
+    public static final String HEADER_STRING = "Authorization";
 }
